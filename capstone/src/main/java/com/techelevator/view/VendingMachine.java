bb3b7aba0c42113ba5a156a6c01fb1cb0eaa68aa
beerobinson <bee.robinson91@gmail.com>
2022-10-03 16:51:50 -0400

    Final Changes

--- capstone/src/main/java/com/techelevator/view/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/view/VendingMachine.java
@@ -9,45 +9,31 @@ public class VendingMachine extends Payment{
 
     Map<String,Item> itemInventory = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
-
-
     public VendingMachine(String pathName){
 
-
         File vend = new File(pathName);
-
 
         try(Scanner dataInput = new Scanner(vend)){
 
             while(dataInput.hasNextLine()){
-
                 //A line of input is itemKey|itemName|price|type
-
                 String lineOfInput = dataInput.nextLine();
                 String[] arr = lineOfInput.split("\\|");
 
                 if(arr[3].equalsIgnoreCase("Candy")){
                     Candy candy = new Candy( Double.parseDouble(arr[2]) , arr[0] , arr[1] ) ;
                     this.itemInventory.put( arr[0] , candy );
-
-
                 }else if(arr[3].equalsIgnoreCase("Gum")){
                     Gum gum = new Gum(Double.parseDouble( arr[2]) , arr[0] , arr[1] ) ;
                     this.itemInventory.put( arr[0] , gum );
-
-
                 }else if(arr[3].equalsIgnoreCase("Chip")){
                     Chip chip = new Chip( Double.parseDouble(arr[2]) , arr[0] , arr[1] ) ;
                     this.itemInventory.put( arr[0] , chip );
-
-
                 }else if(arr[3].equalsIgnoreCase("Drink")){
                     Drink drink = new Drink( Double.parseDouble(arr[2]) , arr[0] ,arr[1] ) ;
                     this.itemInventory.put( arr[0] , drink );
                 }
             }
-
-
         }catch (Exception e){
             System.err.println(e.getMessage());
         }
@@ -67,19 +53,15 @@ public class VendingMachine extends Payment{
 
         int choice = userInput.nextInt();
 
-
         if (choice == 1){
             this.Insert();
             this.Purchase();
-
         }else if (choice == 2){
             this.DispenseItem();
             this.Purchase();
-
         }else if (choice == 3){
             this.ReturnMoney();
             return;
-
         }else{
             System.out.println("That is not a valid choice.");
         }
@@ -91,7 +73,6 @@ public class VendingMachine extends Payment{
     public void DisplayItems(){
 
        try {
-
            for ( Map.Entry<String,Item> item : itemInventory.entrySet() ) {
                item.getValue().DisplayItem();
            }
